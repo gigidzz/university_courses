39e6b3b06d918cc403aef79119040a93e6d7978b
kiu_dzimistarishvili.gio <dzimistarishvili.gio@kiu.edu.ge>
2025-03-20 02:32:15 +0400

    added property types to all functions

--- app/Http/Controllers/API/TssuApiController.php
+++ app/Http/Controllers/API/TssuApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Services\FacultyService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 
@@ -17,41 +18,48 @@ class TssuApiController extends Controller
         $this->facultyService = $facultyService;
     }
 
-    public function index()
+    public function index(): JsonResponse
     {
         $faculties = $this->facultyService->getAllFacultiesPaginated();
+
         return response()->json(['data'=>$faculties],Response::HTTP_OK);
     }
 
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
         $validated = $request->validate([
             'name' => 'required|string|max:255',
             'description' => 'nullable|string',
             'status' => 'required|string|in:active,inactive',
         ]);
+
         $faculty = $this->facultyService->createFaculty($validated);
+
         return response()->json(['data'=>$faculty],Response::HTTP_CREATED);
     }
-    public function show(int $id)
+    public function show(int $id): JsonResponse
     {
         $faculty = $this->facultyService->getFacultyById($id);
+
         return response()->json(['data'=>$faculty],Response::HTTP_OK);
     }
-    public function update(Request $request, int $id)
+    public function update(Request $request, int $id): JsonResponse
     {
         $validated = $request->validate([
             'name' => 'required|string|max:255',
             'description' => 'nullable|string',
             'status' => 'required|string|in:active,inactive',
         ]);
+
         $faculty = $this->facultyService->updateFaculty($id,$validated);
+
         return response()->json(['data'=>$faculty],Response::HTTP_OK);
     }
 
-    public function destroy(int $id)
+    public function destroy(int $id): JsonResponse
     {
         $this->facultyService->deleteFaculty($id);
+
         return response()->json(null,Response::HTTP_NO_CONTENT);
     }
 }
--- app/Http/Controllers/Tssu.php
+++ app/Http/Controllers/Tssu.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Faculty;
 use App\Services\FacultyService;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -22,6 +20,7 @@ class Tssu extends Controller
     public function index(): View
     {
         $faculties = $this->facultyService->getAllFacultiesPaginated();
+
         return view('faculties.index', compact('faculties'));
     }
 
@@ -31,7 +30,6 @@ class Tssu extends Controller
     }
     public function store(Request $request): RedirectResponse
     {
-        // Validate the incoming request data
         $validated = $request->validate([
             'name' => 'required|string|max:255',
             'description' => 'nullable|string',
@@ -45,12 +43,14 @@ class Tssu extends Controller
     public function show(int $id): View
     {
         $faculty = $this->facultyService->getFacultyById($id);
+
         return view('faculties.show', compact('faculty'));
     }
 
     public function edit(int $id): View
     {
         $faculty = $this->facultyService->getFacultyById($id);
+
         return view('faculties.edit', compact('faculty'));
     }
     public function update(Request $request, int $id): RedirectResponse
@@ -76,57 +76,4 @@ class Tssu extends Controller
         return redirect()->route('faculties.index')->with('error', 'Faculty not found!');
     }
 
-
-    public function indexApi(): JsonResponse
-    {
-        $faculties = Faculty::paginate(5);
-        return response()->json($faculties);
-    }
-
-    public function showApi(int $id): JsonResponse
-    {
-        $faculty = Faculty::findOrFail($id);
-        return response()->json($faculty);
-    }
-
-
-    public function storeApi(Request $request): JsonResponse
-    {
-
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'description' => 'nullable|string',
-            'status' => 'required|string|in:active,inactive',
-        ]);
-
-        $faculty = Faculty::create($request->all());
-        return response()->json($faculty, 201); // Return created faculty with status 201
-    }
-
-    public function updateApi(Request $request,int $id): JsonResponse
-    {
-
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'description' => 'nullable|string',
-            'status' => 'required|string|in:active,inactive',
-        ]);
-
-
-        $faculty = Faculty::findOrFail($id);
-        $faculty->update($request->all());
-        return response()->json($faculty);
-    }
-
-    public function destroyApi(int $id): JsonResponse
-    {
-        $faculty = Faculty::find($id);
-
-        if ($faculty) {
-            $faculty->delete();
-            return response()->json(['message' => 'Faculty deleted successfully']);
-        }
-
-        return response()->json(['message' => 'Faculty not found'], 404);
-    }
 }
